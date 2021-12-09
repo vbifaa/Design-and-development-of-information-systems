8e1ff11b15a07af530feb1cc921ed81882887b44
vbifaa <yakubenko.mish@yandex.by>
2021-12-09 20:51:17 +0300

    KKL

--- src/test/java/hello/TestHello.java
+++ src/test/java/hello/TestHello.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class TestHello {
     @Test
-    void test_sum() {
+    public void test_sum() {
         assertEquals(Hello.sum(1,3), 4);
         assertEquals(Hello.sum(5,10), 15);
         assertEquals(Hello.sum(0,3), 3);
